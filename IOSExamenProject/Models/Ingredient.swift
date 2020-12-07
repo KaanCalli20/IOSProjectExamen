80215c6296d59f25b1b0d23e4c62f76bcdef633a
Kaan Calli <kaan.calli@student.hogent.be>
2020-12-07 18:02:14 +0100

    Toevoegen van model klassen

---
+++ IOSExamenProject/Models/Ingredient.swift
@@ -0,0 +1,20 @@
+//
+//  Ingredient.swift
+//  IOSExamenProject
+//
+//  Created by Kaan Calli on 07/12/2020.
+//
+
+import Foundation
+
+class Ingredient {
+    var id: Int
+    var name: String
+    var amount: Double
+    
+    init(id: Int, name:String, amount: Double){
+        self.id = id
+        self.name = name
+        self.amount = amount
+    }
+}
---
+++ IOSExamenProject/Models/Recipe.swift
@@ -0,0 +1,22 @@
+//
+//  Recipe.swift
+//  IOSExamenProject
+//
+//  Created by Kaan Calli on 07/12/2020.
+//
+
+import Foundation
+
+
+class Recipe {
+    let id: Int
+    var name: String
+    var ingredients: [Ingredient]
+    
+    init(id: Int, name: String, ingredients: [Ingredient]) {
+        self.id = id
+        self.name = name
+        self.ingredients = ingredients
+    }
+}
+
